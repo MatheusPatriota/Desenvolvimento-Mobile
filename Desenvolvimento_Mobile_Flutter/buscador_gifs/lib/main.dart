e416bab44a52c0b98dd68ddc9e8e199a914f4bb4
Matheus Patriota <matheus.patriota@ccc.ufcg.edu.br>
2020-01-06 14:50:27 -0300

    aplicativo buscador de gifs adiconado

---
+++ Desenvolvimento_Mobile_Flutter/buscador_gifs/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:buscador_gifs/ui/home_page.dart';
+
+//
+//share: ^0.6.1+1
+//transparent_image: ^1.0.0
+//http: ^0.12.0+2
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
