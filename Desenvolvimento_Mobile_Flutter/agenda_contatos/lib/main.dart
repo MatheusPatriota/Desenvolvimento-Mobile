0901ccfd6f6e8ce900062b01626b86b759e12797
Matheus Patriota <matheus.patriota@ccc.ufcg.edu.br>
2020-01-06 23:46:46 -0300

    app da agenda adicionado

---
+++ Desenvolvimento_Mobile_Flutter/agenda_contatos/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:agenda_contatos/ui/contact_page.dart';
+import 'package:agenda_contatos/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+//
+//sqflite: ^1.1.5
+//url_launcher: ^5.0.2
+//image_picker: ^0.6.0+3
+
+// utilizando sqlite
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
